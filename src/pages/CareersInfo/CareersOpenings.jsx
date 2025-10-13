74393d233556fb113113de05fdcc866b67b37d90
AvanikaGyanaji <avanika.gyanaji@anvi.co>
2025-10-13 10:52:30 +0530

    Update CareersOpenings.jsx

--- src/pages/CareersInfo/CareersOpenings.jsx
+++ src/pages/CareersInfo/CareersOpenings.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import HeroSection from "../../components/heroSection";
+import HeroSection from "../../components/HeroSection";
 import { MapPin, Clock, ChevronDown } from "lucide-react";
 import { jobPositions } from "../../data/jobData";
 import { pagesLinksList } from "../../utils/PagesLinkList";
