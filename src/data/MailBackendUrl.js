2c5e6a476f68a7706fc3d81ed67c95cd103fb992
AvanikaGyanaji <avanika.gyanaji@anvi.co>
2026-04-17 11:45:44 +0530

    Update MailBackendUrl.js to new one

--- src/data/MailBackendUrl.js
+++ src/data/MailBackendUrl.js
@@ -1,5 +1,5 @@
 // 1. Define your base URL as a standalone constant
-const baseUrl = "https://anvimailbackend-bmcka4bdeudxg8be.southindia-01.azurewebsites.net"; "http://192.168.1.50:8000"; 
+const baseUrl = "https://mailsbackend-f9czamgmfzatdxgs.southindia-01.azurewebsites.net"; 
 // const baseUrl = "http://127.0.0.1:8000";
 
 
